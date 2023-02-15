a07210b29f5f33052d737e2431253c6799fc7c64
DenisB7 <denzelbe1987@gmail.com>
2023-02-15 22:54:23 +0500

    Add const

--- javascripts/widgets/linked-checkboxes.js
+++ javascripts/widgets/linked-checkboxes.js
@@ -1,5 +1,6 @@
 function linkedCheckboxes(widget) {
-    
+    const toggleCheckboxes = widget.querySelector('[kjs-role=togglecheckboxes]');
+    const checkIt = widget.querySelectorAll('[kjs-checkbox-id=checkit]');
 }
 
 module.exports = linkedCheckboxes;
