9bacbe69a7dc82b41397d545462c4a71f97117b6
DenisB7 <denzelbe1987@gmail.com>
2023-02-16 21:14:40 +0500

    Refactor linked checkbox in js files

--- javascripts/widgets/linked-checkboxes.js
+++ javascripts/widgets/linked-checkboxes.js
@@ -1,55 +1,61 @@
 function linkedCheckboxes(widget) {
     const controllingCheckbox = widget.querySelector('[kjs-role=controllingcheckbox]');
     const relatedCheckboxes = widget.querySelectorAll('[kjs-role=relatedcheckboxes]');
-
-    function handleClick(e) {
-        let checkedRelatedCount, uncheckAll, setRelatedBool;
-        let relatedCheckboxId = e.target.getAttribute('kjs-relatedcheckbox-id');
-        let controllingCheckboxId = e.target.getAttribute('kjs-controllingcheckbox-id');
-        uncheckAll = false;
-        checkedRelatedCount = 0;
-        for (const checkbox of relatedCheckboxes) {
+  
+    function setup() {
+      controllingCheckbox.addEventListener('click', handleControlCheckboxClick);
+    }
+  
+    function handleControlCheckboxClick(e) {
+      let uncheckAll = false;
+      let setRelatedCheckBoxBoolean = false;
+      for (const checkbox of relatedCheckboxes) {
         if (checkbox.checked) {
-            checkedRelatedCount++;
-            uncheckAll = checkbox.checked;
+          uncheckAll = checkbox.checked;
         }
+      }
+      if (!controllingCheckbox.checked || uncheckAll) {
+        setRelatedCheckBoxBoolean = false;
+        controllingCheckbox.checked = false;
+      }
+      else if (controllingCheckbox.checked) {
+        setRelatedCheckBoxBoolean = true;
+      }
+      for (const check of relatedCheckboxes) {
+        check.checked = setRelatedCheckBoxBoolean;
+      }
+    }
+  
+    function handleRelatedCheckboxClick(e) {
+      let checkedRelatedCount = 0;
+      for (const checkbox of relatedCheckboxes) {
+        if (checkbox.checked) {
+          checkedRelatedCount++;
         }
-        if (relatedCheckboxId) {
-        if (checkedRelatedCount === 0) {
-            controllingCheckbox.checked = false;
-            controllingCheckbox.indeterminate = false;
-        } else if (checkedRelatedCount === relatedCheckboxes.length) {
-            controllingCheckbox.checked = true;
-            controllingCheckbox.indeterminate = false;
-        } else {
-            controllingCheckbox.checked = false;
-            controllingCheckbox.indeterminate = true;
-        }
-        } else if (controllingCheckboxId) {
-        setRelatedBool = false;
-        if (!controllingCheckbox.checked || uncheckAll) {
-            setRelatedBool = false;
-            controllingCheckbox.checked = false;
-        }
-        else if (controllingCheckbox.checked) {
-            setRelatedBool = true;
-        }
-        for (const check of relatedCheckboxes) {
-            check.checked = setRelatedBool;
-        }
-        }
+      }
+      if (checkedRelatedCount === 0) {
+        controllingCheckbox.checked = false;
+        controllingCheckbox.indeterminate = false;
+      } else if (checkedRelatedCount === relatedCheckboxes.length) {
+        controllingCheckbox.checked = true;
+        controllingCheckbox.indeterminate = false;
+      } else {
+        controllingCheckbox.checked = false;
+        controllingCheckbox.indeterminate = true;
+      }
     }
     var actions = [];
-    [...relatedCheckboxes, controllingCheckbox].forEach(function (checkbox) {
-        actions.push({
+    relatedCheckboxes.forEach(function (checkbox) {
+      actions.push({
         element: checkbox,
         event: 'click',
-        handler: handleClick
-        });
+        handler: handleRelatedCheckboxClick
+      });
     });
     return {
-        actions: actions
+      setup: setup,
+      actions: actions
     };
 }
-
+  
 module.exports = linkedCheckboxes;
